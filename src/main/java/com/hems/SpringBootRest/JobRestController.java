ae807497d78328dc0519b5da1fdabe17c2f451b5
Hemanth2003m <22p31a05f1@acet.ac.in>
2025-08-16 17:10:13 +0530

    First commit

---
+++ src/main/java/com/hems/SpringBootRest/JobRestController.java
@@ -0,0 +1,38 @@
+package com.hems.SpringBootRest;
+
+import com.hems.SpringBootRest.model.JobPost;
+import com.hems.SpringBootRest.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class JobRestController {
+    @Autowired
+    private JobService service;
+    @GetMapping("jobPosts")
+   // @ResponseBody
+    public List<JobPost> getAllJobs(){
+        return service.getAllJobs();
+    }
+
+    @GetMapping("jobPost/{postId}")
+    //@ResponseBody
+    public JobPost getJob(@PathVariable("postId") int postId){
+        return service.getJob(postId);
+    }
+    @PostMapping("jobPost")
+    public void addJob(@RequestBody JobPost jobPost){
+        service.addJob(jobPost);
+    }
+    @PutMapping("jobPost")
+    public void updateJob(@RequestBody JobPost jobPost){
+        service.updateJob(jobPost);
+    }
+    @DeleteMapping("jobPost/{postId}")
+    public void deleteJob(@PathVariable int postId){
+        service.deleteJob(postId);
+    }
+}
---
+++ src/test/java/com/hems/SpringBootRest/SpringBootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hems.SpringBootRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
